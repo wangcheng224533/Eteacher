b3b9c0df364d46ce9357065fbe3485ccc33dd923
Wang Cheng <2533919886@qq.com>
2018-02-23 14:12:29 +0800

    add MainActivity

--- app/src/main/java/com/eteacher/wangc/eteacher/activity/MainActivity.java
+++ app/src/main/java/com/eteacher/wangc/eteacher/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.eteacher.wangc.eteacher;
+package com.eteacher.wangc.eteacher.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.eteacher.wangc.eteacher.R;
 
 public class MainActivity extends AppCompatActivity {
 
